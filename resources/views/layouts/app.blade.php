a120705964607e7ea72bad953b7c1d60957dc76b
Krish033 <srik51977@gmail.com>
2025-09-23 12:43:23 +0530

    Initial Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,16 +8,15 @@
 
     <!-- Google Font -->
     <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('admin/plugins/fontawesome-free/css/all.min.css') }}">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" />
     <!-- AdminLTE style -->
-    <link rel="stylesheet" href="{{ asset('admin/css/adminlte.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('admin/css/adminlte.min.css') }}">
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="{{ asset('admin/css/custom.css')}}">
+    <link rel="stylesheet" href="{{ asset('admin/css/custom.css') }}">
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine.js"></script>
 
 
@@ -25,60 +25,62 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
+
 <body class="hold-transition sidebar-mini bg-gray-100">
-<div class="wrapper">
+    <div class="wrapper">
 
 
 
-    <div class="flex justify-start items-start">
+        <div class="flex justify-start items-start">
 
-    @include('layouts.sidebar')
+            @include('layouts.sidebar')
 
-        <div class="flex flex-col w-full p-2">
-
-
-    <nav class="bg-white w-full border-b-1 border-gray-200 px-5 py-3 shadow-sm rounded-xl">
-
-        <a class=""  href="#" role="button">
-            <i class="fas fa-bars"></i>
-        </a>
-
-    </nav>
-    <!-- /.navbar -->
-
-    <!-- Sidebar -->
+            <div class="flex flex-col w-full p-2">
 
 
-    <!-- Content -->
-    <div class="content-wrapper p-6">
-        @yield('content')
-    </div>
+                <nav class="bg-white w-full border-b-1 border-gray-200 px-5 py-3 shadow-sm rounded-xl">
+
+                    <a class="" href="#" role="button">
+                        <i class="fas fa-bars"></i>
+                    </a>
+
+                </nav>
+                <!-- /.navbar -->
+
+                <!-- Sidebar -->
+
+
+                <!-- Content -->
+                <div class="content-wrapper p-6">
+                    @yield('content')
+                </div>
+            </div>
         </div>
     </div>
-</div>
-<!-- ./wrapper -->
+    <!-- ./wrapper -->
 
-<!-- jQuery -->
-<script src="{{ asset('admin/plugins/jquery/jquery.min.js') }}"></script>
+    <!-- jQuery -->
+    <script src="{{ asset('admin/plugins/jquery/jquery.min.js') }}"></script>
 
-<!-- Bootstrap 4 -->
-<script src="{{ asset('admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
-<!-- AdminLTE App -->
-<script src="{{ asset('admin/js/adminlte.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('admin/js/adminlte.min.js') }}"></script>
 
-<!-- Custom JS -->
-<script src="{{ asset('admin/js/demo.js') }}"></script>
+    <!-- Custom JS -->
+    <script src="{{ asset('admin/js/demo.js') }}"></script>
 
-<script>
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    });
-</script>
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
 
-@yield('customJs')
-@yield('customCss')
+    @yield('customJs')
+    @yield('customCss')
 </body>
+
 </html>
